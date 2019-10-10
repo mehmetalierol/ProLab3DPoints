b66554198fe0f4e5b8f5e219c5a0cf6162c9794a
Mehmet Ali EROL <mehmetalierol@windowslive.com>
2019-10-10 10:07:58 +0300

    Sabitler için header eklendi.

---
+++ consts.h
@@ -0,0 +1,18 @@
+#ifndef CONSTS_H_INCLUDED
+#define CONSTS_H_INCLUDED
+
+//Proje içerisinde kullanýlacak sabit deðerler
+const char *FileExtention = ".nkt";
+const char *DirectoryPath = "C://DataFiles";
+const char *CheckAscii = "ascii";
+
+//hata mesajlarý
+const char *FilePointCountWrong = " dosyasindaki nokta sayisi gecerli degildir.\n";
+const char *FileOpenError = "Dosya acilamadi!.";
+const char *DirectoryOpenError = "Dizin acilamadi.";
+const char *ApplicationDirectoryError = "Uygulama dizini alinamadi!";
+const char *NoFileInDirectoryError = "Dizinde hic %s dosyasi bulunamadi!";
+const char *LineNoRgbError = " Nokta verisi r g b bilgileri olmadan verilmistir.";
+const char *LineRgbError = " Nokta verisi r g b bilgileri ile verilmistir. Dosyada r g b alanlari yoktur.";
+
+#endif // CONSTS_H_INCLUDED
